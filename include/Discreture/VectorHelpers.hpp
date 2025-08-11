eafe006f906e9fa6f79eec0ddd76eeb7de1676e0
Rusty Conover <rusty@conover.me>
2025-08-10 21:01:08 -0400

    fix: comment out specializations

--- include/Discreture/VectorHelpers.hpp
+++ include/Discreture/VectorHelpers.hpp
@@ -14,38 +14,38 @@ namespace discreture
 ////////////////////////////////////
 /// \brief prints out a space separated std::vector.
 ////////////////////////////////////
-template <class T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& V)
-{
-    std::copy(V.begin(), V.end(), std::ostream_iterator<T>(os, " "));
+// template <class T>
+// std::ostream& operator<<(std::ostream& os, const std::vector<T>& V)
+// {
+//     std::copy(V.begin(), V.end(), std::ostream_iterator<T>(os, " "));
 
-    return os;
-}
+//     return os;
+// }
 
 ////////////////////////////////////
 /// \brief prints out a vector of vector's as a matrix
 ////////////////////////////////////
-template <class T>
-std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& V)
-{
-    os << std::endl;
-    for (auto&& v : V)
-        os << v << std::endl;
+// template <class T>
+// std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& V)
+// {
+//     os << std::endl;
+//     for (auto&& v : V)
+//         os << v << std::endl;
 
-    return os;
-}
+//     return os;
+// }
 
 /////////////////////////////
 /// \brief Specialization for std::vector printouts for std::vector<bool> so
 /// that it doesn't print out spaces between 0's and 1's
 /////////////////////////////
-inline std::ostream& operator<<(std::ostream& os, const std::vector<bool>& V)
-{
-    for (bool v : V)
-        os << v;
+// inline std::ostream& operator<<(std::ostream& os, const std::vector<bool>& V)
+// {
+//     for (bool v : V)
+//         os << v;
 
-    return os;
-}
+//     return os;
+// }
 
 ///////////////////////////
 /// \brief Function composition
